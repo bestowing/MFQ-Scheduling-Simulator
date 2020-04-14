0cc097f555eace40afc62db84877b25ec57cc2e6
bestowing <bestowing@naver.com>
2020-04-15 06:53:55 +0900

    주석 정리 및 코드 최적화

--- os.c
+++ os.c
@@ -1,36 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX 5
 
 /*
 ** Data
 */
 
-//프로세스 구조체 선언
+// 프로세스 구조체 선언
 typedef struct Process {          
-    int PID;                      //Identification number of the process
-    int queue;                    //Initial queue
-    int arr_t;                    //Arrival time
-    int cycle_num;                //cycle number
-    int cycle_index;              //index for cycle array
-    int* seq_burst;               //integer array for burst time
+    int PID;                      // Identification number of the process
+    int queue;                    // Initial queue
+    int arr_t;                    // Arrival time
+    int cycle_num;                // Cycle number
+    int cycle_index;              // Index for cycle array
+    int* seq_burst;               // Integer array storing burst time
 } Process;
 
-//연결리스트 구조체 선언
+// 연결리스트 구조체 선언
 typedef struct Node {
     struct Node* next;
     Process* data;
 } Node;
 
-//전역변수
-Process *  job_queue    [MAX]  ;  //ready queue에 arrival하기 전 프로세스
-Node    *  ready_queue0        ;  //Q0, RR(time quantum = 2)
-Node    *  ready_queue1        ;  //Q1, RR(time quantum = 6)
-Process *  ready_queue2 [MAX]  ;  //Q2, SRTN
-Node    *  ready_queue3        ;  //Q3, FCFS
-Process *  sleep_queue  [MAX]  ;  //I/O를 요청한 프로세스
+// 전역변수
+Process **  job_queue;            // ready queue에 arrival하기 전 프로세스
+Node    *   ready_queue0;         // Q0, RR(time quantum = 2)
+Node    *   ready_queue1;         // Q1, RR(time quantum = 6)
+Process **  ready_queue2;         // Q2, SRTN
+Node    *   ready_queue3;         // Q3, FCFS
+Process **  sleep_queue;          // I/O를 요청한 프로세스
+int process_num;                  // 스케줄링할 프로세스 총 개수
 int global_time = 0;
-int process_num;                  //스케줄링할 프로세스 개수
 int time_quantum;                 
 
 
@@ -38,7 +37,7 @@ int time_quantum;
 ** Function
 */
 
-//프로세스 포인터에 메모리를 할당하고 입력받은 프로세스 정보를 넣음
+// 프로세스 포인터에 메모리를 할당하고 입력받은 프로세스 정보를 넣음
 Process* set_process(int _PID, int _queue, int _arr_t, int _cycle_num) {
     Process* new_process = (Process*)malloc(sizeof(Process));
     int arr_size;
@@ -52,16 +51,38 @@ Process* set_process(int _PID, int _queue, int _arr_t, int _cycle_num) {
     return new_process;
 }
 
-//입력받은 프로세스의 메모리를 해제하는 함수
+// 입력받은 프로세스의 메모리를 해제하는 함수
 void delete_process(Process* process) {
-    free(process->seq_burst);  //burst time을 저장하는 배열
+    free(process->seq_burst);  // burst time을 저장하는 배열
     free(process);
     return;
 }
 
-//input.txt 파일을 읽어 프로세스 정보를 입력받음
-//입력받은 정보로 set_process() 함수 호출, 메모리 할당
-void init_process() {
+// Q0, Q1, Q2, Q3와 job_queue, sleep_queue에 메모리 할당
+void init_queue() {
+    job_queue = (Process**)malloc(sizeof(Process) * process_num);
+    ready_queue0 = (Node*)malloc(sizeof(Node));
+    ready_queue0->data = NULL;
+    ready_queue0->next = NULL;
+    ready_queue1 = (Node*)malloc(sizeof(Node));
+    ready_queue1->data = NULL;
+    ready_queue1->next = NULL;
+    ready_queue2 = (Process**)malloc(sizeof(Process) * process_num);
+    for(int i=0; i<process_num; i++) {
+        ready_queue2[i] = NULL;
+    }
+    ready_queue3 = (Node*)malloc(sizeof(Node));
+    ready_queue3->data = NULL;
+    ready_queue3->next = NULL;
+    sleep_queue = (Process**)malloc(sizeof(Process) * process_num);
+    for(int i=0; i<process_num; i++) {
+        sleep_queue[i] = NULL;
+    }
+}
+
+// input.txt 파일을 읽어 사용자로부터 정보를 입력받음
+// 입력받은 정보로 프로세스와 레디큐에 메모리 할당하여 생성함
+void set_simulation() {
     FILE* file = fopen("input.txt", "r");
     int pid, init_q, arr_t, cycle;
     int size_arr;
@@ -71,6 +92,7 @@ void init_process() {
         return;
     }
     fscanf(file, "%d", &process_num);
+    init_queue();
     for (int i = 0; i < process_num; i++) {
         fscanf(file, "%d %d %d %d", &pid, &init_q, &arr_t, &cycle);
         size_arr = (cycle * 2) - 1;
@@ -85,20 +107,7 @@ void init_process() {
     return;
 }
 
-//연결리스트인 Q0, Q1, Q3에 메모리를 할당
-void init_queue() {
-    ready_queue0 = (Node*)malloc(sizeof(Node));
-    ready_queue0->data = NULL;
-    ready_queue0->next = NULL;
-    ready_queue1 = (Node*)malloc(sizeof(Node));
-    ready_queue1->data = NULL;
-    ready_queue1->next = NULL;
-    ready_queue3 = (Node*)malloc(sizeof(Node));
-    ready_queue3->data = NULL;
-    ready_queue3->next = NULL;
-}
-
-//입력받은 프로세스의 남은 burst time을 반환하는 함수
+// 입력받은 프로세스의 남은 burst time을 반환하는 함수
 int get_burst_time(Process* process) {
     return process->seq_burst[process->cycle_index];
 }
@@ -124,51 +133,10 @@ int cpu_running(Process* process) {
     return 1;
 }
 
-/*
-void check_process() {
-    int total = 0;
-    printf("Q0(");
-    Node * head = ready_queue0;
-    while(head->next != NULL) {
-        printf("%d ", head->next->data->PID);
-        head = head->next;
-        total++;
-    }
-    printf(") Q1(");
-    head = ready_queue1;
-    while(head->next != NULL) {
-        printf("%d ", head->next->data->PID);
-        head = head->next;
-        total++;
-    }
-    printf(") Q2(");
-    for(int i=0; i<process_num; i++) {
-        if(ready_queue2[i] != NULL) {
-            printf("%d ", ready_queue2[i]->PID);
-            total++;
-        }
-    }
-    printf(")Q3 (");
-    head = ready_queue3;
-    while(head->next != NULL) {
-        printf("%d ", head->next->data->PID);
-        head = head->next;
-        total++;
-    }
-    printf(") 슬립큐 (");
-    for(int i=0; i<process_num; i++) {
-        if(sleep_queue[i] != NULL) {
-            printf("%d ", sleep_queue[i]->PID);
-            total++;
-        }
-    }
-    printf(") 총: %d개 프로세스 ", total);
-}
-*/
-
+// 입력받은 프로세스를 해당하는 큐에 push함
 void push_queue(Process* process) {
     Node* head;
-    //해당 프로세스가 어느 큐에 들어갈지 확인
+    // 해당 프로세스가 어느 큐에 들어갈지 확인
     int queue_num = process->queue;
     int pid = process->PID - 1;
     switch (queue_num) {
@@ -197,18 +165,18 @@ void push_queue(Process* process) {
     return;
 }
 
-// I/O burst time이 종료된 프로세스 확인
+// I/O burst time이 종료된 프로세스를 확인하고 레디큐에 넣음
 void io_check() {
     for (int i = 0; i < process_num; i++) {
         if (sleep_queue[i] != NULL) {
             int time = get_burst_time(sleep_queue[i]);
-            // 종료된 프로세스라면
             if (time == 0) {
-                // 우선순위를 하나 올리고 레디큐에 넣는다
+                // I/O burst를 마친 프로세스는 우선순위를 하나 올림
                 sleep_queue[i]->cycle_index += 1;
                 int queue = sleep_queue[i]->queue;
                 queue = queue > 0 ? queue - 1 : 0;
                 sleep_queue[i]->queue = queue;
+                // 레디큐에 넣음
                 push_queue(sleep_queue[i]);
                 sleep_queue[i] = NULL;
             }
@@ -216,7 +184,7 @@ void io_check() {
     }
 }
 
-// job queue에 남아있는 프로세스의 arrival 확인
+// job queue에 남아있는 프로세스의 arrival time을 확인하고 레디큐에 넣음
 // 남아있는 프로세스가 있으면 1, 없으면 0 반환
 int arrival_check() {
     int result = 0;
@@ -230,13 +198,12 @@ int arrival_check() {
             }
         }
     }
-    if (result == 1) {
-        return 1;
-    } else {
-        return 0;
-    }
+    return result;
 }
 
+// 입력받은 프로세스의 burst time을 확인,
+// I/O를 요청한 경우 sleep_queue로 보냄
+// 아직 시간이 남았다면 1, I/O를 요청했으면 0, 종료되었으면 -1을 반환
 int burst_check(Process* process, int queue, int PID) {
     int result = cpu_running(process);
     //I/O 요청
@@ -247,8 +214,8 @@ int burst_check(Process* process, int queue, int PID) {
     return result;
 }
 
-// 해당 레디큐에서 스케줄링하여 프로세스 반환
-// 없으면 NULL 반환
+// 입력받은 레디큐 (Q0 or Q1 or Q3)에서 스케줄링하여 프로세스 반환
+// 반환할 프로세스가 없으면 NULL 반환
 Process* fcfs(int type) {
     Process* result;
     Node* head;
@@ -274,13 +241,13 @@ Process* fcfs(int type) {
     remove = head->next;
     result = remove->data;
     head->next = remove->next;
-    
+    // 스케줄링한 프로세스는 레디큐에서 제거
     free(remove);
     return result;
 }
 
-// 해당 레디큐에서 스케줄링하여 프로세스 반환
-// 없으면 NULL 반환
+// 레디큐 (Q2)에서 스케줄링하여 프로세스 반환
+// 반환할 프로세스가 없으면 NULL 반환
 Process* srtn() {
     Process* result = NULL;
     int min_time = -1;
@@ -302,10 +269,8 @@ Process* srtn() {
     return result;
 }
 
-// 현재 burst_time보다 짧은 프로세스가 있다면
-// preemtion 발생
-// 없으면 NULL 반환
-// preemption 발생하면 우선순위 하나 낮춤
+// 레디큐 Q2에서, 현재 실행중인 프로세스의 burst_time보다 짧은 burst_time을 가진 프로세스를 확인하고 반환함( preemption 발생 )
+// 반환할 프로세스가 없으면 NULL 반환
 Process* preemtion(int burst_time) {
     Process* result = NULL;
     int min_time = burst_time;
@@ -329,8 +294,7 @@ Process* preemtion(int burst_time) {
     return NULL;
 }
 
-// 슬립큐에 프로세스가 하나라도 있으면 1 반환
-// 없으면 0 반환
+// sleep_queue에 프로세스가 하나라도 있으면 1, 없으면 0 반환함
 int sleep_check() {
     for (int i = 0; i < process_num; i++) {
         if (sleep_queue[i] != NULL) {
@@ -340,6 +304,8 @@ int sleep_check() {
     return 0;
 }
 
+// 입력받은 프로세스를 asleep 상태로 만들어줌
+// 즉, sleep_queue로 보냄
 void sleep(Process* process) {
     int pid = process->PID;
     pid -= 1;
@@ -347,52 +313,51 @@ void sleep(Process* process) {
     return;
 }
 
-// 레디큐에 프로세스가 있으면 우선순위에 따라 프로세스 반환
-// 레디큐와 슬립큐 모두 없으면 NULL 반환
+// 우선순위를 고려한 스케줄링으로 프로세스 반환
+// 레디큐 Q0, Q1, Q2, Q3가 모두 비었다면 NULL 반환
 Process* scheduling() {
     Process* result = NULL;
-
-    //Queue 0 확인
+    // Queue 0 확인
     result = fcfs(0);
     if (result != NULL) {
         time_quantum = 2;
         return result;
     }
-
-    //Queue 1 확인
+    // Queue 1 확인
     result = fcfs(1);
     if (result != NULL) {
         time_quantum = 6;
         return result;
     }
-    //Queue 2 확인
+    // Queue 2 확인
     result = srtn();
     if (result != NULL) {
         time_quantum = -1;
         return result;
     }
-    //Queue 3 확인
+    // Queue 3 확인
     result = fcfs(3);
     if (result != NULL) {
         time_quantum = -1;
         return result;
     }
-    //아무런 프로세스가 존재하지 않음
+    // 반환할 프로세스가 없음( 레디큐가 모두 비었음 )
     return result;
 }
 
+// 시뮬레이션을 시작함
 void start_simulation() {
-    Process* current_process = NULL;
+    Process* current_process = NULL; // cpu자원을 받은 프로세스
     int current_process_id = 0;
     int current_queue = -1;
     int prev_process_id = 0;
     int remain_process = 1;
+    // 모든 프로세스가 종료될때까지 반복함
     while (1) {
-        // I/O burst time 종료된 프로세스 확인 -> 슬립큐에서 레디큐로 옮김
+        // I/O burst time 종료된 프로세스 확인, sleep_queue에서 ready_queue로 넣음
         io_check();
 
-        // job_queue에 남아있는 프로세스가 있는지 확인
-        // 모든 프로세스가 arrive하면 실행하지 않음
+        // job_queue에 남아있는 프로세스가 있는지 확인, 모든 프로세스가 arrive할때까지 확인함
         if (remain_process == 1) {
             int arrival_result = arrival_check();
             if (arrival_result == 0) {
@@ -400,36 +365,34 @@ void start_simulation() {
             }
         }
 
-        // 실행중인 프로세스 있는지 확인 
+        // 실행중인 프로세스 있는지 확인, 새로 스케줄링함
         if (current_process == NULL) {
-            // 없으면 새로 스케쥴링
             current_process = scheduling();
-            // 새로 스케줄링한 프로세스가 없으면 
             if (current_process == NULL) {
                 int check = sleep_check();
-                // 남아있는 프로세스와 슬립큐를 확인함
-                // 둘 다 없으면 종료함
                 if (remain_process == 0 && check == 0) {
+                    // 모든 프로세스가 종료된 것을 확인하면 반복문 종료
                     break;
                 }
-                // 하나라도 남았으면 대기함
+                // I/O를 기다리는 프로세스만 남은 경우, cpu가 그냥 대기함
                 current_process_id = 0;
-                current_queue = 9; //나중에 수정
+                current_queue = -1;
             } else {
-                // 정보를 갱신함
+                // 새로 스케줄링한 프로세스의 정보로 갱신함
                 current_process_id = current_process->PID;
                 current_queue = current_process->queue;
             }
         }
-        // 실행중인 프로세스 있는 경우,
+        // 실행중인 프로세스 있는 경우
         else {
-            // 실행중인 프로세스가 Q2에서 온 프로세스인지 확인 -> Q2 다시 검사
+            // 실행중인 프로세스가 SRTN 기법인 경우, preemption 확인함
             if (current_queue == 2) {
                 Process* preemtion_process;
                 int burst_time = get_burst_time(current_process);
                 preemtion_process = preemtion(burst_time);
-                // 강탈 발생함
+                // preemption 발생
                 if (preemtion_process != NULL) {
+                    // 실행중이던 프로세스는 Q3으로 진입
                     current_process->queue = 3;
                     push_queue(current_process);
                     current_process = preemtion_process;
@@ -439,13 +402,19 @@ void start_simulation() {
             }
         }
         
+        // cpu를 할당받은 프로세스가 변경되는 경우, 출력함
         if(prev_process_id != current_process_id) {
-            printf("%5d %5d\n", global_time, current_process_id);
+            if(current_process_id == 0) {
+                // cpu가 실행중인 프로세스가 없는 경우
+                printf("%5d %5c\n", global_time, ' ');
+            } else {
+                printf("%5d %5d\n", global_time, current_process_id);
+            }
             prev_process_id = current_process_id;
         }
-        
-        //check_process();
-        // global time을 1증가 (cpu running)
+
+        // cpu의 time quantum이 1 지남
+        // I/O 대기중이던 프로세스의 burst time도 1씩 감소시킴
         global_time += 1;
         time_quantum -= 1;
         for (int i = 0; i < process_num; i++) {
@@ -455,19 +424,20 @@ void start_simulation() {
             }
         }
 
+        // I/O를 기다리는 프로세스만 남아서 실행중인 프로세스 없는 경우, 그냥 넘어감
         if( current_process == NULL) continue;
 
-        // 실행중인 프로세스의 burst time을 1 줄임
-        // -> 끝났으면 실행중인 프로세스에서 제거하기
-        // -> I/O 요청했으면 슬립큐에 넣기
+        // 실행중인 프로세스의 cpu burst time을 1 줄임
+        // cpu burst time이 끝났으면 실행중인 프로세스에서 제거하고 ready_queue에 넣음
+        // - I/O 요청했으면, 슬립큐에 넣기
+        // - 프로세스가 종료되었으면 삭제함
         int check = burst_check(current_process, current_queue, current_process_id);
-        // 프로세스가 종료되거나 I/O를 요청함
         if (check != 1) {
             current_process = NULL;
             current_process_id = 0;
             current_queue = -1;
         } else {
-            //타임 퀀텀을 다썼는지 확인 -> 우선순위 낮추고 레디큐 진입
+            // time quantum을 모두 소모하고 ready_queue로 돌아가는 경우, 우선순위를 1 낮춤
             if (time_quantum == 0) {
                 current_queue = current_queue < 3 ? current_queue + 1 : 3 ;
                 current_process->queue = current_queue;
@@ -496,9 +466,18 @@ void print_info() {
 }
 */
 
+// 큐에 할당한 메모리를 모두 반환함
+void delete_queue() {
+    free(job_queue);
+    free(ready_queue0);
+    free(ready_queue1);
+    free(ready_queue2);
+    free(ready_queue3);
+    free(sleep_queue);
+}
+
 int main() {
-    init_process();
-    init_queue();
-    //print_info();
-    start_simulation();
+    set_simulation();   // 시뮬레이션 세팅
+    start_simulation(); // 시뮬레이션 시작
+    delete_queue();     // 자원 반납
 }
